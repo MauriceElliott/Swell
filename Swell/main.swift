dc779fd8609183701f49ae6f485d9ce09df4717b
MauriceElliott <mauricemelliott@gmail.com>
2025-01-20 00:36:56 +0000

    Continued

--- Swell/main.swift
+++ Swell/main.swift
@@ -12,25 +12,31 @@ print("Swell")
 var runSwell = true
 
 let environment = [
-	"TERM": "xterm",
-	"HOME": "/Users/mauriceelliott",
-	"PATH": "Users/mauriceelliott/.bun/bin:/Users/mauriceelliott/path_src:/usr/local/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/Apple/usr/bin:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Applications/Ghostty.app/Contents/MacOS"
+  "TERM": "xterm",
+  "HOME": "/Users/mauriceelliott",
+  "PATH":
+    "Users/mauriceelliott/.bun/bin:/Users/mauriceelliott/path_src:/usr/local/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/Apple/usr/bin:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Applications/Ghostty.app/Contents/MacOS",
 ]
 
 while runSwell {
-	let input = readLine()!.split(separator: " ")
-	let command = input.first!
-	
-	if(command == "exit"){
-		runSwell = false
-	} else {
-		let process = Process()
-		process.launchPath = "/usr/local/bin/fish"
-		process.environment = environment
-		process.arguments = ["", String(command)]
-		try! process.run()
-		let pipe = Pipe()
-		process.standardError = pipe
-		process.standardOutput = pipe
-	}
+  let input = readLine()!.split(separator: " ")
+  let command = input.first!
+
+  if command == "exit" {
+    runSwell = false
+  } else {
+    let process = Process()
+    process.launchPath = "/bin/sh"
+    process.environment = environment
+    process.arguments = ["-c", String(command)]
+    try! process.run()
+    let pipe = Pipe()
+    process.standardError = pipe
+    process.standardOutput = pipe
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8) ?? ""
+
+    print(output)
+  }
 }
