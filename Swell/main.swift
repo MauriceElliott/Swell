9018e1b8368f3e467ab7d113f39c6f9fe7120069
MauriceElliott <mauricemelliott@gmail.com>
2025-01-20 00:31:57 +0000

    Start of Swell, not sure what's going on yet but enjoying myself

--- Swell/main.swift
+++ Swell/main.swift
@@ -7,4 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Swell")
+
+var runSwell = true
+
+let environment = [
+	"TERM": "xterm",
+	"HOME": "/Users/mauriceelliott",
+	"PATH": "Users/mauriceelliott/.bun/bin:/Users/mauriceelliott/path_src:/usr/local/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/Apple/usr/bin:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Applications/Ghostty.app/Contents/MacOS"
+]
+
+while runSwell {
+	let input = readLine()!.split(separator: " ")
+	let command = input.first!
+	
+	if(command == "exit"){
+		runSwell = false
+	} else {
+		let process = Process()
+		process.launchPath = "/usr/local/bin/fish"
+		process.environment = environment
+		process.arguments = ["", String(command)]
+		try! process.run()
+		let pipe = Pipe()
+		process.standardError = pipe
+		process.standardOutput = pipe
+	}
+}
