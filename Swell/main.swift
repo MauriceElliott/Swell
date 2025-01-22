2916e996c2fbed036285e84958ae749d31ab2cf2
MauriceElliott <mauricemelliott@gmail.com>
2025-01-22 06:55:45 +0000

    need to switch to posix_spawnp

--- Swell/main.swift
+++ Swell/main.swift
@@ -18,21 +18,24 @@ let environment = [
     "Users/mauriceelliott/.bun/bin:/Users/mauriceelliott/path_src:/usr/local/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/Apple/usr/bin:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Applications/Ghostty.app/Contents/MacOS",
 ]
 
-while runSwell {
-  let input = readLine()!.split(separator: " ")
-  let command = input.first!
 
-  if command == "exit" {
+while runSwell {
+  let process = Process()
+  print("\u{001B}[3;32m 󰶟 => \u{001B}[0;39m", terminator: "")
+  let input = readLine()
+  let pipe = Pipe()
+
+  if input == "exit" {
     runSwell = false
   } else {
-    let process = Process()
-    process.launchPath = "/bin/sh"
+    process.executableURL = URL.init(filePath: "/bin/ls")
     process.environment = environment
-    process.arguments = ["-c", String(command)]
-    try! process.run()
-    let pipe = Pipe()
+    process.arguments = ["-c", String(input!)]
     process.standardError = pipe
     process.standardOutput = pipe
+
+    try! process.run()
+    
 
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     let output = String(data: data, encoding: .utf8) ?? ""
