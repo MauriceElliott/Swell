0eabe90353a6d435951cc022b30d7d0caf242a9e
MauriceElliott <mauricemelliott@gmail.com>
2025-02-03 13:52:22 +0000

    source tidy

--- Swell/commands/addAlias.swift
+++ Swell/commands/addAlias.swift
@@ -1,5 +1,3 @@
-//in memory aliases, read in main when parsing new commands
-//updated when loading config or the alias command is called.
 
 func addAlias(alias: [String]) {
     _session.aliases[alias[1]] = Command(command: alias[2], arguments: Array(alias[2...]))
