b37711a27119ffa08045ddc1da885f277fcf8a9c
MauriceElliott <mauricemelliott@gmail.com>
2025-02-11 23:13:40 +0000

    debugging

--- Swell/core/tabComplete.swift
+++ Swell/core/tabComplete.swift
@@ -9,7 +9,7 @@ func tabComplete(fuzz: String) -> String {
             return startsWith.first!.replacingOccurrences(of: fuzz, with: "")
         }
     } else if fuzz.split(separator: " ").count > 1 || fuzz.last == " " {
-        
+        //to be continued, this needs to autocomplete paths
     }
     
     return fuzz
