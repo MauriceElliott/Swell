964d583f9b6457def06425a1cfa72e77a735dcc1
MauriceElliott <maurice.elliott@telusagcg.com>
2025-06-23 13:28:57 +0100

    Added a Package.swift file

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Swell",
+    platforms: [
+        .macOS(.v13),
+        .linux(.v5_8)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .executable(
+            name: "swell",
+            targets: ["Swell"]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "Swell",
+            dependencies: [],
+            path: "Swell",
+            sources: [
+                "main.swift",
+                "core/readInput.swift",
+                "core/spawnProcess.swift", 
+                "core/session.swift",
+                "core/getPrompt.swift",
+                "core/getAvailableCommands.swift",
+                "core/sanitiseInput.swift",
+                "commands/changeDirectory.swift",
+                "config/readConfig.swift"
+            ]
+        ),
+        .testTarget(
+            name: "SwellTests",
+            dependencies: ["Swell"]
+        ),
+    ]
+)
