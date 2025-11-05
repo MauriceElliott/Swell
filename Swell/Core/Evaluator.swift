9b7e414e2ded6410de6e8467813abc844a7b3cf6
MauriceElliott <mauricemelliott@gmail.com>
2025-11-05 21:33:50 +0000

    fixed history issue

--- Swell/Core/Evaluator.swift
+++ Swell/Core/Evaluator.swift
@@ -11,6 +11,8 @@ func evaluate(node: ASTNode, state: inout SessionState) {
 				default:
 					spawnProcess(command: cmd.command, arguments: cmd.arguments, state: state)
 			}
+			state.history.append(cmd)
+			state.historyIndex += 1
 		case .pipeline(let nodes):
         	for node in nodes {
 				evaluate(node: node, state: &state)	
--- Swell/Core/getPrompt.swift
+++ Swell/Core/getPrompt.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func getPrompt(state: borrowing SessionState) -> String {
-    let currentDirectory = FileManager.default.currentDirectoryPath
-    let directoryFromHome = currentDirectory.replacingOccurrences(of: state.homeDir, with: "~/")
+    let directoryFromHome = state.curDir.replacingOccurrences(of: state.homeDir, with: "~/")
     return "\u{001B}[3;32m 󰶟  \(directoryFromHome) => \u{001B}[0;39m"
 }
