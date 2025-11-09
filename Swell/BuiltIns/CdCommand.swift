f5e62f802a0b21c30969f7ae67f62007f500327d
MauriceElliott <mauricemelliott@gmail.com>
2025-11-09 23:36:02 +0000

    Fixed issue with cd

--- Swell/BuiltIns/CdCommand.swift
+++ Swell/BuiltIns/CdCommand.swift
@@ -11,30 +11,31 @@ struct CdCommand: BuiltInCommand {
     	        print("Failed to go home.")
     	        state.curDir = fm.currentDirectoryPath
     	    }
-    	}
-    	switch args[1] {
-    	case "~":
-    	    _ = fm.changeCurrentDirectoryPath(state.homeDir)
-    	    state.curDir = state.homeDir
-    	case "..":
-    	    // TODO Needs to change to recursion so in the instance that there is a second .. we can just call change directory again and have it deal with the change.
-    	    let currentDirList = state.curDir.split(separator: "/")
-    	    if let sectionToRemove = currentDirList.last {
-    	        let newDir = state.curDir.replacingOccurrences(of: "/\(sectionToRemove)", with: "")
-    	        if fm.changeCurrentDirectoryPath(newDir) {
-    	            state.curDir = newDir
-    	        }
-    	    } else {
-    	        state.curDir = fm.currentDirectoryPath
-    	    }
-    	case "/":
-    	    _ = fm.changeCurrentDirectoryPath(args[1])
-    	    state.curDir = fm.currentDirectoryPath
-    	default:
-    	    let newDir = state.curDir + "/" + args[1]
-    	    if fm.changeCurrentDirectoryPath(newDir) {
-    	        state.curDir = newDir
-    	    }
-    	}
+    	} else {
+	    	switch args[1] {
+	    	case "~":
+	    	    _ = fm.changeCurrentDirectoryPath(state.homeDir)
+	    	    state.curDir = state.homeDir
+	    	case "..":
+	    	    // TODO Needs to change to recursion so in the instance that there is a second .. we can just call change directory again and have it deal with the change.
+	    	    let currentDirList = state.curDir.split(separator: "/")
+	    	    if let sectionToRemove = currentDirList.last {
+	    	        let newDir = state.curDir.replacingOccurrences(of: "/\(sectionToRemove)", with: "")
+	    	        if fm.changeCurrentDirectoryPath(newDir) {
+	    	            state.curDir = newDir
+	    	        }
+	    	    } else {
+	    	        state.curDir = fm.currentDirectoryPath
+	    	    }
+	    	case "/":
+	    	    _ = fm.changeCurrentDirectoryPath(args[1])
+	    	    state.curDir = fm.currentDirectoryPath
+	    	default:
+	    	    let newDir = state.curDir + "/" + args[1]
+	    	    if fm.changeCurrentDirectoryPath(newDir) {
+	    	        state.curDir = newDir
+	    	    }
+	    	}
+		}		
 	}
 }
