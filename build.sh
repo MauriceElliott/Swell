f103716837d9f3c669d890e98013410ec97b4b2d
MauriceElliott <mauricemelliott@gmail.com>
2025-11-09 23:30:26 +0000

    Updated build again

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 swift build -c release 2>&1 | grep -v "no version information available"
-cp $(swift build -c release --show-bin-path 2>&1 | grep -v "no version information available")/swell /usr/local/bin/swell
+sudo cp $(swift build -c release --show-bin-path 2>&1 | grep -v "no version information available")/swell /usr/local/bin/swell
