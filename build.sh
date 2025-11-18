a52eb4eca8a0a007d8297da9ad57120bb8d39af1
MauriceElliott <maurice.elliott@telusagcg.com>
2025-11-18 13:07:03 +0000

    Fixed the build script so it works on my WSL (where I am the root user)

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 
 swift build -c release 2>&1 | grep -v "no version information available"
-sudo cp $(swift build -c release --show-bin-path 2>&1 | grep -v "no version information available")/swell /usr/local/bin/swell
+if [[ $(id -u) == 0 ]]; then
+  cp $(swift build -c release --show-bin-path 2>&1 | grep -v "no version information available")/swell /usr/local/bin/swell
+else
+  sudo cp $(swift build -c release --show-bin-path 2>&1 | grep -v "no version information available")/swell /usr/local/bin/swell
+fi
