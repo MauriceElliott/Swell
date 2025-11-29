7e5f6b260892b1d35c8d39b6c0beac17ee1bcb41
MauriceElliott <mauricemelliott@gmail.com>
2025-11-29 22:38:53 +0000

    Fixed aliases! They work again, had to add back getAlias function though which probably needs a better permanent home but one for another day

--- Sources/Core/Evaluator.swift
+++ Sources/Core/Evaluator.swift
@@ -4,6 +4,9 @@ func evaluate(node: ASTNode, state: inout SessionState) {
 			if let builtIn = BuiltInRegistry().get(name: cmd.command) {
 				builtIn.run(args: cmd.arguments, state: &state)
 			} else {
+				if let aliasedCmd = getAlias(cmd: cmd, state: state) {
+					spawnProcess(command: aliasedCmd.command, arguments: aliasedCmd.arguments, state: state)
+				}
 				spawnProcess(command: cmd.command, arguments: cmd.arguments, state: state)
 			}
 			state.history.append(cmd)
